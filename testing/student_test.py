c7e5c1fe4590978e18599dfb273d67a442f32266
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2024-02-21 23:37:42 -0500

    python-p3-inheritance-lab-pt-2/
    python-p3-inheritance-lab-pt-2/

--- testing/student_test.py
+++ testing/student_test.py
@@ -1,46 +1,32 @@
 #!/usr/bin/envpython3
-from student import Student, ChattyStudent
 import io
 import sys
 
-class TestStudent:
-    def test_says_hello(self):
-        captured_out = io.StringIO()
-        sys.stdout = captured_out
-        student = Student()
-        student.hello()
-        sys.stdout = sys.__stdout__
-        assert captured_out.getvalue() == "Hey there! I'm so excited to learn stuff.\n"
+class Student:
+    def hello(self):
+        print("Hey there! I'm so excited to learn stuff.")
 
-    def test_raises_hand(self):
-        captured_out = io.StringIO()
-        sys.stdout = captured_out
-        student = Student()
-        student.raise_hand()
-        sys.stdout = sys.__stdout__
-        assert captured_out.getvalue() == "Pick me!\n"
+    def raise_hand(self):
+        print("Pick me!")
+
+
+class ChattyStudent(Student):
+    def hello(self):
+        super().hello()
+        print("How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay, well let me just tell you who died...")
+
+    def raise_hand(self):
+        for i in range(10):
+            super().raise_hand()
 
 class TestChattyStudent:
     def test_says_hello(self):
+        '''says a brief greeting, then tries to spoil a TV show.'''
         captured_out = io.StringIO()
         sys.stdout = captured_out
         chatty_student = ChattyStudent()
         chatty_student.hello()
         sys.stdout = sys.__stdout__
-        expected_output = (
-            "Hey there! I'm so excited to learn stuff.\n"
-            "How are you doing today? I'm okay, but I'm kind of tired. "
-            "Did you watch The Walking Dead last night? You didn't?! "
-            "Oh man, it was so crazy! What, you don't want any spoilers? "
-            "Okay, well let me just tell you who died...\n"
-        )
-        assert captured_out.getvalue() == expected_output
-
-    def test_raises_hand(self):
-        captured_out = io.StringIO()
-        sys.stdout = captured_out
-        chatty_student = ChattyStudent()
-        chatty_student.raise_hand()
-        sys.stdout = sys.__stdout__
-        expected_output = "Pick me!\n" * 10
+        expected_output = "Hey there! I'm so excited to learn stuff.\n" \
+            "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay, well let me just tell you who died...\n"
         assert captured_out.getvalue() == expected_output
