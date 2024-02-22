5e8fdfaca70f2310a35a8b8501ce349b9fb11aed
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2024-02-21 23:40:37 -0500

    python-p3-inheritance-lab-pt-2/
    python-p3-inheritance-lab-pt-2/

--- testing/student_test.py
+++ testing/student_test.py
@@ -30,3 +30,13 @@ class TestChattyStudent:
         expected_output = "Hey there! I'm so excited to learn stuff.\n" \
             "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay, well let me just tell you who died...\n"
         assert captured_out.getvalue() == expected_output
+
+    def test_raise_hand(self):
+        '''respectfully tries to get the teacher's attention ten times.'''
+        captured_out = io.StringIO()
+        sys.stdout = captured_out
+        chatty_student = ChattyStudent()
+        chatty_student.raise_hand()
+        sys.stdout = sys.__stdout__
+        expected_output = "Pick me!\n" * 10
+        assert captured_out.getvalue() == expected_output
